b81fff439e09ecac1965f1b29c7ed04bdce5ec91
Alexander-Fernando <alexander.glzprd@hotmail.com>
2020-08-28 22:06:36 -0500

    mi segundo commit

---
+++ views/contenido.view.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--PARA CARGAR LAS FUENTES ESPECIALES--->
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Contenedor</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1 class="titulo">PÁGINA PRINCIPAL - IDSW GRUPO 06</h1>
+        <a href="cerrar.php">Cerrar Sesion</a>
+        <hr class="border">
+        <div class="contenido">
+            <article>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores laudantium minus possimus velit laborum, a sit! Similique cumque, quaerat, distinctio veritatis, alias tempore repellat aspernatur laudantium omnis repudiandae modi perspiciatis tempora? Rem, obcaecati consequuntur.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius aperiam illo magnam cum neque officiis culpa quia iste sit quibusdam ducimus ex quasi voluptatum expedita sequi fugiat dignissimos nam nisi officia minus, modi nobis voluptatem! Optio commodi labore voluptas nulla, perspiciatis corrupti, ducimus cupiditate dignissimos debitis impedit, officia tempora quo.</p>
+
+            </article>
+        </div>
+    </div>
+    
+</body>
+</html>
